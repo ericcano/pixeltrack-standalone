40abde015f3b51aebb71b6296724c3e294cd8519
Eric Cano <eric.cano@cern.ch>
2021-09-01 16:49:00 +0200

    Moved deferrable cuda calls outside of critical sections when possible.
    The practical performance gain on cudadev is neverthless negligible.

--- src/cudadev/CUDACore/deviceAllocatorStatus.h
+++ src/cudadev/CUDACore/deviceAllocatorStatus.h
@@ -7,16 +7,13 @@ namespace cms {
   namespace cuda {
     namespace allocator {
       struct TotalBytes {
-        size_t free;
-        size_t live;
-        size_t liveRequested;  // CMS: monitor also requested amount
-        TotalBytes() { free = live = liveRequested = 0; }
+        size_t free = 0;
+        size_t live = 0;
+        size_t liveRequested = 0;  // CMS: monitor also requested amount
       };
       /// Map type of device ordinals to the number of cached bytes cached by each device
       using GpuCachedBytes = std::map<int, TotalBytes>;
     }  // namespace allocator
-
-    allocator::GpuCachedBytes deviceAllocatorStatus();
   }  // namespace cuda
 }  // namespace cms
 
